99cd2e6c3b6212824b190c73d6343f1273c31fef
rakaguntur <rakaguntur33@gmail.com>
2019-10-11 20:19:28 +0700

    penambahan index

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+  <head>
+    <title>TUGAS 1 101162xx-11</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/custom-bs.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="fonts/line-icons/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body id="top">
+
+
+      <!-- <div id="ininet"></div> -->
+
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+
+    <!-- NAVBAR -->
+    <header class="site-navbar mt-3">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="site-logo col-6"><a href="index.html">MASTER</a></div>
+          <div class="right-cta-menu text-right d-flex aligin-items-center col-10">
+            <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu h3 m-0 p-0 mt-2"></span></a>
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+    <!-- HOME -->
+    <section class="home-section section-hero overlay bg-image" id="ininet">
+
+
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-12">
+            <div class="mb-5 text-center">
+              <h1 class="text-white font-weight-bold">MASTER</h1>
+
+            </div>
+			<center>
+        <table>
+          <tr>
+        <td><h4 class="text-white"> Cefi Amidin M | </h4></td>
+        <td><h4 class="text-white"> 10116220 | </h4></td>
+      </tr>
+              </div>
+			  </center>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="js/three.r92.min.js"></script>
+    <script src="js/vanta.net.min.js"></script>
+    <script>
+        VANTA.NET({
+            el: "#ininet"
+        })
+    </script>
+  </body>
+</html>
